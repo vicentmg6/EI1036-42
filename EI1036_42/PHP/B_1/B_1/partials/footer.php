1dda5edfc9834729e8f30bc5a1b406849732963a
Javier Mollón <98605792+Jobo32@users.noreply.github.com>
2023-10-14 11:41:47 +0200

    Update footer.php

--- EI1036_42/PHP/B_1/B_1/partials/footer.php
+++ EI1036_42/PHP/B_1/B_1/partials/footer.php
@@ -2,8 +2,8 @@
 	<img src="https://licensebuttons.net/l/by-sa/3.0/88x31.png" alt="licencia" />
 	<time datetime="2023-09-18">2023</time><br/>
 	<address>
-		<p class="izq"> El footer editado
-			<a href="mailto:webmaster@example.com" rev="author">Vicent Marti</a>.</p>
+		<p class="izq"> Página creada por
+			<a href="mailto:al405660@uji.es" rev="author">Vicent Marti & Javier Mollón</a>.</p>
 		<p class="der"> Universitat Jaume I </p>
 	</address>
 </footer>
