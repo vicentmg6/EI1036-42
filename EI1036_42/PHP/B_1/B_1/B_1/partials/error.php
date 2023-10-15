f305861d7125ba6e7a5f2d21bffe46093b78f554
Javier Mollón <98605792+Jobo32@users.noreply.github.com>
2023-10-15 13:58:54 +0200

    Delete EI1036_42/PHP/B_1/B_1/B_1 directory

--- EI1036_42/PHP/B_1/B_1/B_1/partials/error.php
+++
@@ -1,4 +0,0 @@
-<main>
-	<h1>Algo no ha ido bien</h1>
-	<p>Vuelve a intentarlo con otra opción </p>
-</main>
--- EI1036_42/PHP/B_1/B_1/B_1/partials/home.php
+++
@@ -1,6 +0,0 @@
-
-<main>
-	<h2>Bienvenidos</h2>
-	<p>Visita todas nuestras seciones </p>
-	<img src="./media/centro.png" id="logo2" alt="logo2" />
-</main>
