73bf61e6558bb877cf0046e6fb65c31956187bb8
Javier Mollón <98605792+Jobo32@users.noreply.github.com>
2023-10-15 14:23:24 +0200

    Update header.php

--- EI1036_42/PHP/B_1/B_1/partials/header.php
+++ EI1036_42/PHP/B_1/B_1/partials/header.php
@@ -1,3 +1,15 @@
+/**
+ * * Descripción: Fichero HTML/PHP que es el encabezado de la página web.
+ * *
+ * * Descripción extensa: Muestra el encabezado de la página web, el título de la página, el logo del centro y el favicon.
+ * *
+ * * @author  Javier <al404921@uji.es>  Vicent <al405660@uji.es>
+ * * @copyright 2023 Javier y Vicent
+ * * @license http://www.fsf.org/licensing/licenses/gpl.txt GPL 2 or later
+ * * @version 2
+ * *
+ **/
+	 
 <!DOCTYPE html>
 <html lang="es">
 
