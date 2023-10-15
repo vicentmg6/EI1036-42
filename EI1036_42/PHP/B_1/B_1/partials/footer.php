98c2f1687e700b2eaac15cbca40cb6d529ce0b79
Javier Mollón <98605792+Jobo32@users.noreply.github.com>
2023-10-15 14:22:04 +0200

    Update footer.php

--- EI1036_42/PHP/B_1/B_1/partials/footer.php
+++ EI1036_42/PHP/B_1/B_1/partials/footer.php
@@ -1,3 +1,15 @@
+/**
+ * * Descripción: Fichero HTML/PHP que representa el pie de la página.
+ * *
+ * * Descripción extensa: Se muestran los autores, el derecho de copyright y el centro eduactivo de los autores.
+ * *
+ * * @author  Javier <al404921@uji.es>  Vicent <al405660@uji.es>
+ * * @copyright 2023 Javier y Vicent
+ * * @license http://www.fsf.org/licensing/licenses/gpl.txt GPL 2 or later
+ * * @version 2
+ * *
+ **/
+
 <footer>
 	<img src="https://licensebuttons.net/l/by-sa/3.0/88x31.png" alt="licencia" />
 	<time datetime="2023-09-18">2023</time><br/>
